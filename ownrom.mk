8c5fd7671776c702c6dfd659006d6ff852fc833e
vilinfield <vilinfield@gmail.com>
2017-05-12 18:29:13 +0000

    ownrom setup

--- ownrom.mk
+++ ownrom.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OwnROM stuff.
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := ownrom_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
